8a896e11d1aaa42de0d9f21124497213048b8307
JiWon <jiohning00@hanmail.net>
2021-03-10 11:51:50 +0900

    로그인 페이지 작업중

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Users' });
 });
 
+router.get('/sign_in', (req, res) => {
+  res.render('users/signin', {});
+});
+
 module.exports = router;
