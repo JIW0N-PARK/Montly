75b711188f21dc4bde196f56d76be00d6f27093a
JiWon <jiohning00@hanmail.net>
2021-03-23 00:28:34 +0900

    상품 등록 페이지 작업 중

--- routes/products.js
+++ routes/products.js
@@ -11,4 +11,8 @@ router.get('/new', catchErrors(async (req, res, next) => {
   res.render('partners/products/new');
 }));
 
+router.post('/new', catchErrors(async (req, res, next) => {
+  console.log("POST NEW FORM");
+}));
+
 module.exports = router;
